f0d0847bf5e243ec41c5c9aa5e77a1ffb281c5a6
GinMu <gin.musheng@gmail.com>
2024-03-21 15:18:19 +0800

    update config

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 const nodes = [
   // "http://54.223.58.54:50333",
   // "http://180.76.51.249:5050",
-  "http://39.101.176.68:5050"
+  // "http://39.101.176.68:5050",
+  // "https://srje115qd43qw2.swtc.top",
+  "https://srje071qdew231.jccdex.cn"
 ];
 
 exports.nodes = nodes;
